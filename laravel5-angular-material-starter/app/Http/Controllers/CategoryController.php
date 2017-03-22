827b3e25dbbd90f342a59d4642379ddd6ef5b222
Quentin Claudel <quentin.claudel@me.com>
2017-03-22 16:39:25 +0100

    Ajoute les fonctions de mise a jour, de suppression et d'ajout au controleur de categorie

--- laravel5-angular-material-starter/app/Http/Controllers/CategoryController.php
+++ laravel5-angular-material-starter/app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Webpatser\Uuid\Uuid;
 
 use App\Category;
 use App\Event;
@@ -61,5 +62,67 @@ class CategoryController extends Controller
             else
                 return response()->error('Aucun événement trouvé.', 204);
         }
+    }
+    
+    /**
+     * Methode permettant de creer une categorie
+     * route : /api/categories
+     * methode : POST
+     */
+    public function create(Request $request) {
+        $this->validate($request, [
+            'title' => 'required|unique:category',
+            'description' => 'required'
+        ]);
+        
+        $category = new Category();
+        $category->id = Uuid::generate();
+        $category->title = trim($request->title);
+        $category->description = trim($request->description);
+        
+        $category->save();
+
+        return response()->json($category);
+    }
+    
+    /**
+     * Methode permettant de mettre a jour une categorie
+     * route : /api/categories/{id}
+     * methode : PUT
+     */
+    public function update(Request $request, $id) {
+        $categorie = Category::find($id);
+        
+        if($categorie != null) {
+            $this->validate($request, [
+                'title' => 'required|unique:category',
+                'description' => 'required'
+            ]);
+            
+            echo $request->title;
+            
+            $categorie->title = trim($request->title);
+            $categorie->description = trim($request->description);
+            $categorie->save();
+            
+            return response()->json($category);
+        } else 
+            return response()->error('Aucune catégorie correspondant à l\'identifiant n\'a été trouvée.', 204);
+    }
+    
+    /**
+     * Methode permettant de supprimer une categorie
+     * route : /api/categories/{id}
+     * methode : DELETE
+     */
+    public function delete(Request $request, $id) {
+        $categorie = Category::find($id);
+        
+        if($categorie != null) {
+            $categorie->delete();
+            
+            return response()->success('La catégorie a bien été supprimée.');
+        } else 
+            return response()->error('Aucune catégorie correspondant à l\’identifiant n\'a été trouvée.', 204);
     }
 }
