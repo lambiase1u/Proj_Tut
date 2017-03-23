0116d735e076e23cb11b793df0e4cd771d70f046
lambiase1u <davidlambiase@gmail.com>
2017-03-24 00:24:23 +0100

    Profile user css

--- laravel5-angular-material-starter/angular/app/components/user/user.component.js
+++ laravel5-angular-material-starter/angular/app/components/user/user.component.js
@@ -1,17 +1,32 @@
-class UserController{
-    constructor(){
+class UserController {
+    constructor(API, $scope, ToastService, $log) {
         'ngInject';
 
-        //
+        this.API = API;
+        this.$scope = $scope;
+        this.ToastService = ToastService;
+        this.$log = $log;
     }
 
-    $onInit(){
+    $onInit() {
+        this.$scope.arrayData = [
+            {src: 'http://www.francetvinfo.fr/image/75e490efj-61bb/260/146/12104700.jpg'},
+            {src: 'http://www.francetvinfo.fr/image/75e490efj-61bb/260/146/12104700.jpg'},
+            {src: 'http://www.francetvinfo.fr/image/75e490efj-61bb/260/146/12104700.jpg'}
+        ];
+
+        this.$log.log(this.$scope.arrayData);
+
+        this.$scope.slideIndex = 1;
+        $("#slider").slick();
     }
+
+
 }
 
 export const UserComponent = {
     templateUrl: './views/app/components/user/user.component.html',
     controller: UserController,
-    controllerAs: 'vm',
+    controllerAs: 'userCtrl',
     bindings: {}
 }
