e5547a533895a1e6e56df2e6bebc93c29f58d595
maxime <maxks.w@gmail.com>
2017-03-26 19:52:17 +0200

    Modification de la page d'accueil , les évènements de la bdd s'affichent , on peut uniquement  y participer ( si ce n'est pas déjà le cas )

--- laravel5-angular-material-starter/angular/app/components/events/events.component.js
+++ laravel5-angular-material-starter/angular/app/components/events/events.component.js
@@ -1,14 +1,50 @@
 class EventsController{
-    constructor(){
+    constructor(API,$log,$state,ToastService){
         'ngInject';
 
        this.place = null;
+       this.API = API;
+       this.$log = $log;
+       this.events = [];
+       this.$state = $state;
+       this.ToastService = ToastService;
     }
 
     $onInit(){
 
+        this.API.all('events').get('').then((response) => {
+            this.$log.log(response.data.listEvents);
+            this.events = response.data.listEvents;
+
+        });
 
 
+
+    }
+
+    participate(id){
+
+
+        this.$log.log("participe button");
+        let toast = this.ToastService;
+
+
+        var data = {
+        };
+
+        this.API.all('events/'+id+'/participate').post(data).then((response) => {
+            this.ToastService.show("Vous participate désormais à l'évènement");
+
+
+        }).catch(function(response) {
+
+           // this.$log.log(response);
+            toast.error("Vous y participatez déjà ");
+        });
+    }
+
+    event_create(){
+        return this.$state.go('app.event_create');
     }
 
 
--- laravel5-angular-material-starter/angular/config/routes.config.js
+++ laravel5-angular-material-starter/angular/config/routes.config.js
@@ -106,13 +106,26 @@ export function RoutesConfig($stateProvider, $urlRouterProvider) {
                 }
             }
         })
-        .state('app.event', {
-            url: '/event/create',
+        .state('app.event_create', {
+            url: '/events/create',
             data: {auth: true},//{auth: true} would require JWT auth
             views: {
                 'main@': {
                     templateUrl: getView('event')
                 }
             }
-        });
+        })
+        .state('app.event_id', {
+        url: '/events/{id:(?:[a-fA-F0-9]{8}(?:-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12})}',
+        data: {auth: true},//{auth: true} would require JWT auth
+        views: {
+            'main@': {
+                templateUrl: getView('event')
+            }
+        }
+        })
+
+
+
+    ;
 }
