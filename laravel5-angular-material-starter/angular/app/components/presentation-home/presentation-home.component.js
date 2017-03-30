780ad4cbe4246926771bf7a98d77d08daad5b4d9
lambiase1u <davidlambiase@gmail.com>
2017-03-31 00:49:18 +0200

    Bloque a propos sur la page d'accueil

---
+++ laravel5-angular-material-starter/angular/app/components/presentation-home/presentation-home.component.js
@@ -0,0 +1,17 @@
+class PresentationHomeController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const PresentationHomeComponent = {
+    templateUrl: './views/app/components/presentation-home/presentation-home.component.html',
+    controller: PresentationHomeController,
+    controllerAs: 'vm',
+    bindings: {}
+}
