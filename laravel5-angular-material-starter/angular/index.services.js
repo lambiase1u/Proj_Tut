db7b5056397877536a95783440adf4ec5b838d7d
Quentin Claudel <quentin.claudel@me.com>
2017-03-31 18:36:18 +0200

    Ajoute le service utilisateur

--- laravel5-angular-material-starter/angular/index.services.js
+++ laravel5-angular-material-starter/angular/index.services.js
@@ -1,3 +1,4 @@
+import {UserService} from './services/User.service';
 import {CategoryService} from './services/Category.service';
 import {EventService} from './services/Event.service';
 import {APIService} from './services/API.service';
@@ -5,6 +6,7 @@ import {DialogService} from './services/dialog.service';
 import {ToastService} from './services/toast.service';
 
 angular.module('app.services')
+	.service('UserService', UserService)
 	.service('CategoryService', CategoryService)
 	.service('EventService', EventService)
 	.service('API', APIService)
