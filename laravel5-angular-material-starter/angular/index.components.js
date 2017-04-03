773881baba12fccb4576a007e457eff0476cda75
lambiase1u <davidlambiase@gmail.com>
2017-04-03 12:05:59 +0200

    delete useless components

--- laravel5-angular-material-starter/angular/index.components.js
+++ laravel5-angular-material-starter/angular/index.components.js
@@ -2,7 +2,6 @@ import {PresentationHomeComponent} from './app/components/presentation-home/pres
 import {BannerHomeComponent} from './app/components/banner-home/banner-home.component';
 import {DateComponent} from './app/components/date/date.component';
 import {EventContentComponent} from './app/components/eventContent/eventContent.component';
-import {EventsComponent} from './app/components/events/events.component';
 import {HeaderComponent} from './app/components/header/header.component';
 import {UsersComponent} from './app/components/users/users.component';
 import {EventFormComponent} from './app/components/eventForm/eventForm.component';
@@ -17,7 +16,6 @@ angular.module('app.components')
 	.component('bannerHome', BannerHomeComponent)
 	.component('date', DateComponent)
 	.component('eventContent', EventContentComponent)
-	.component('events', EventsComponent)
 	.component('header', HeaderComponent)
 	.component('users', UsersComponent)
 	.component('eventForm', EventFormComponent)
