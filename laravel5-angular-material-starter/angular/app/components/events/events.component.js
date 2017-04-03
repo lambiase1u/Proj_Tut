a77ae6041cea9ee459c79a0b5ab8a4b87e51bcfa
demougin2u <mini-d@hotmail.fr>
2017-04-03 19:37:50 +0200

    Ajout d'une liste de cartes d'événements sur la page d'accueil

--- laravel5-angular-material-starter/angular/app/components/events/events.component.js
+++ laravel5-angular-material-starter/angular/app/components/events/events.component.js
@@ -1,11 +1,10 @@
-let vm;
-
 class EventsController{
-    constructor(UserService,$log,$state,ToastService,NgMap){
+    constructor(UserService, EventService,$log,$state,ToastService,NgMap){
         'ngInject';
 
        this.place = null;
        this.UserService = UserService;
+       this.EventService = EventService;
        this.$log = $log;
        this.event = null;
        this.events = [];
@@ -13,7 +12,6 @@ class EventsController{
        this.ToastService = ToastService;
        this.user = null;
        this.map = NgMap;
-       vm=this;
        this.location = { 
             lat : 0,
             lng : 0
@@ -23,9 +21,43 @@ class EventsController{
     }
 
     $onInit(){
-        this.UserService.getLocation().then((success) => {
-            this.location = success.data.location;
-        });
+      //permet d'initialiser la position de la map
+      this.UserService.getLocation().then((success) => {
+          this.location = success.data.location;
+      });
+
+      //On initialise events
+      this.EventService.findAll().then((success) => {
+          this.events = success.data.listEvents;
+          this.events.forEach((event) => {
+            this.positions.push({lat: event.lat, lng: event.lng});
+            this.extractDate(event);
+          });
+      });
+    }
+
+    extractDate(event){
+      event.dateDebut = this.formatDate(event.dateDebut);
+      event.dateFin = this.formatDate(event.dateFin);
+      event.sameDay = (event.dateDebut.getDate() === event.dateFin.getDate());
+      console.log(event.sameDay);
+    }
+
+    formatDate(dateString){
+      var dateSplit = dateString.split(' ');
+      var date = dateSplit[0];
+      var time = dateSplit[1];
+      var dateTime = new Date(date);
+      var timeSplit = time.split(':');
+      var hours = timeSplit[0];
+      var minutes = timeSplit[1];
+      dateTime.setHours(hours);
+      dateTime.setMinutes(minutes);
+      return dateTime;
+    }
+
+    seeEvent(id){
+      this.$state.go('app.event_details', {id: id});
     }
 }
 
